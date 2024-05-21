c579dc4999954606b0fb7a2fe9d9c635432ccf7a
lexmin0412 <zhangle_dev@outlook.com>
2024-05-21 23:17:23 +0800

    feat: 列表项新增所属分组名展示

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -9,7 +9,8 @@ export default function List(props: ListProps) {
     <div className="w-full px-20">
       <div className="mb-2 text-base flex items-center font-semibold border-b border-b-slate-700 border-solid border-x-0 border-t-0 pb-2">
         <div className="w-96 ellipsis-single ">标题</div>
-        <div className="ml-3">预计结束时间</div>
+        <div className="ml-3 w-24">预计结束时间</div>
+        <div className="ml-3">所属分组</div>
       </div>
 
       {props.data.map((item) => {
@@ -21,7 +22,8 @@ export default function List(props: ListProps) {
                   className="mb-2 text-base flex items-center"
                 >
                   <div className="w-96 ellipsis-single ">{child.title}</div>
-                  <div className="ml-3">{child.expected_end_at}</div>
+                  <div className="ml-3 w-24">{child.expected_end_at}</div>
+                  <div className="ml-3 ellipsis-single w-48">{item.title}</div>
                 </div>
               );
             })
